bd20d292afc41e961a7e4ab3c35c0646aec67839
Morgan <103436411+MorganKryze@users.noreply.github.com>
2023-04-29 15:42:10 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using System.Text.Json;
+namespace Cookinguest;
+
+public class Program
+{
+    public static MySqlConnection connection = InitializeConnection();
+    public static void Main()
+    {
+        connection.Open();
+
+    }
+
+    public static MySqlConnection InitializeConnection()
+    {
+        string jsonString = File.ReadAllText("settings.json");
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+        Dictionary<string,string>? dico = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonString, options);
+        if (dico is null)
+            throw new NullReferenceException("The dictionary is null.");
+        return new MySqlConnection($"server={dico["server"]};port={dico["port"]};database={dico["database"]};user={dico["user"]};password={dico["passord"]};"); 
+    }
+}
